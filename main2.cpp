d1ac3bdeb8bc252ca2eb0bee96e28f9232aded6c
Carl130013 <318261818@qq.com>
2026-09-26 11:32:31 +0800

    插值查找

--- main2.cpp
+++ main2.cpp
@@ -1,24 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-int FINDDATA(int example[], int len, int num)
-{
-	for (int i = 0;i < len;i++)
-	{
-		if (example[i] = num)
-			{
-				return example [i];
-			}
-	}
-	return -1;
-}
 int main()
 {
-	int arr[] = { 13,14,79,98,94,2,6,161,151 };
-	int len = sizeof(arr) / sizeof(int);
-	int num = 13;
-	FINDDATA(arr, len, num);
-	int c = FINDDATA(arr, len, num);
-	printf("%d\n", c);
-	return 0;
+	int arr[] = { 22,12,13,35,28,24,47,42,49 };
+	int maxindex[] = { 22,35,49 };
+	int start[] = { 0,3,6 };
+	int key = 24;
+	int i, j;
+	for ( i = 0;i < 3; i++)
+	{
+		if (maxindex[i] < key)
+		{
+			continue;
+		}
+		if (maxindex[i] >= key)
+		{
+			
+			break;
+
+		}
+	}
+	int begin = start[i];
+	int end;
+	if (i + 1 < 3)//证明还存在第三组，将end设置为第三组第一个数据即可
+	{
+		end = start[i + 1];
+	}
+	for (j = begin;j < end;j++)
+	{
+		if (arr[j] == key)
+		{
+			
+			printf("%d\n", j);
+			return j;
+		}
+		else
+		{
+			continue;
+		}
+	}
+
 }
+
+
+
+
+
+
